904397aebdf45a2aee5fc7739a1bd3b28310dbd8
David Falusi <falusi.david94@gmail.com>
2019-10-18 23:50:02 +0200

    Add center container component

---
+++ src/components/CenterContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="center-container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CenterContainer',
+};
+</script>
+
+<style lang="scss" scoped>
+  .center-container {
+    padding: 1.5rem 0.8rem;
+    width: inherit;
+    max-width: 768px;
+
+    @media screen and (min-width: 768px) {
+      margin: 0 auto;
+    }
+  }
+</style>
--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="layout">
     <Header />
-    <div class="content"></div>
+    <CenterContainer>
+      Hello Vue!
+    </CenterContainer>
   </div>
 </template>
 
 <script>
 import Header from './Header.vue';
+import CenterContainer from './CenterContainer.vue';
 
 export default {
   name: 'MainLayout',
   components: {
     Header,
+    CenterContainer,
   },
 };
 </script>
