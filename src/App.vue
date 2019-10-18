8724e3e0d7ba3bcb7efcfab748dcb269d2edc37d
David Falusi <falusi.david94@gmail.com>
2019-10-18 22:20:56 +0200

    Add main layout

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div id="app">
+    <MainLayout />
   </div>
 </template>
 
 <script>
+import MainLayout from './components/MainLayout.vue';
 
 export default {
   name: 'app',
   components: {
+    MainLayout,
   },
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
 }
 </style>
---
+++ src/components/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="layout">
+    <div class="content"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  components: {
+  },
+};
+</script>
