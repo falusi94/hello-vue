e03e1f914386f8aa258c96a3c7597dd594946771
David Falusi <falusi.david94@gmail.com>
2019-10-19 17:27:45 +0200

    Create abstract button

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    class="base-button"
+    @click="$emit('click')">
+    {{ title }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: [
+    'title',
+  ],
+};
+</script>
+
+<style lang="scss" scoped>
+  .base-button {
+    padding: 1rem 1rem 0.5rem 1rem;
+    font-size: inherit;
+    font-weight: inherit;
+    background: none;
+    color: inherit;
+    border: none;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+  }
+</style>
