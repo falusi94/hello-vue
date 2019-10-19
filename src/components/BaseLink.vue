5dbee8915b20fb12c693ee3b84df7f5082ff3807
David Falusi <falusi.david94@gmail.com>
2019-10-19 18:37:09 +0200

    Add link component

---
+++ src/components/BaseLink.vue
@@ -0,0 +1,34 @@
+<template>
+  <a
+    class="base-link"
+    @click="$emit('click')">
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'BaseLink',
+};
+</script>
+
+<style lang="scss" scoped>
+  .base-link {
+    &, &:link, &:visited, &:hover, &:active {
+      text-decoration: none;
+    }
+
+    &, &:link {
+      color: #607D8B;
+    }
+
+    &:visited {
+      color: #455A64;
+    }
+
+    &:hover, &:active {
+      color: #263238;
+      transition-duration: 0.5s;
+    }
+}
+</style>
