21b3c7191c3391d94d96464dd67f1dbd365a962e
David Falusi <falusi.david94@gmail.com>
2020-08-29 19:25:23 +0200

    Add suspend like loader

---
+++ src/components/BaseSuspend.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if=(isLoading)>
+    <TheSpinner/>
+  </div>
+  <div v-else>
+    <slot />
+  </div>
+</template>
+
+<script>
+import TheSpinner from './TheSpinner.vue';
+
+export default {
+  name: 'BaseSuspend',
+  components: {
+    TheSpinner,
+  },
+  props: {
+    isLoading: Boolean,
+  },
+};
+</script>
